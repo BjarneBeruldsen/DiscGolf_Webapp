a7da9fd4e387b0cfe90e96c250c9295001f2fb7b
Bjarne Beruldsen <beruldsen.bjarne@gmail.com>
2025-01-24 14:38:40 +0100

    lagt til lasting

--- disk-applikasjon/frontend/src/LagKlubbSide.js
+++ disk-applikasjon/frontend/src/LagKlubbSide.js
@@ -1,13 +1,19 @@
 import { useState } from 'react';   
+import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const LagKlubbSide = () => {
 
     const [klubbnavn, setKlubbnavn] = useState('');
     const [kontaktinfo, setKontaktinfo] = useState('');
+    const [laster, setLaster] = useState(false);
+    const minne = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault(); 
         const klubb = {klubbnavn, kontaktinfo}; 
+
+        setLaster(true); 
         
         fetch('http://localhost:8000/klubber', {
             method: 'POST',
@@ -15,10 +21,13 @@ const LagKlubbSide = () => {
             body: JSON.stringify(klubb)
         }).then(() => {
             console.log('Ny klubb lagt til');
+            setLaster(false);
             alert('Ny klubb lagt til');
+            minne.push('/');
         })
         .catch(error => {
             console.error('Feil ved lagring av klubb:', error);
+            setLaster(false); 
         })
     }
 
@@ -39,7 +48,8 @@ const LagKlubbSide = () => {
                     required
                     onChange={(e) => setKontaktinfo(e.target.value)}
                 />
-                <button>Legg til klubb</button>
+                {!laster && <button>Legg til klubb</button>}
+                {laster && <button>Legger til klubb..</button>}
             </form>
         </div>
      );
