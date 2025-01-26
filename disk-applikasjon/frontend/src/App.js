0c0be394f6e0a38482a3625e85d9bb16369c27b8
Abdinasir909 <abdinasirali24@gmail.com>
2025-01-27 00:23:51 +0100

    lagt til Header-komponent

--- disk-applikasjon/frontend/src/App.js
+++ disk-applikasjon/frontend/src/App.js
@@ -1,11 +1,14 @@
 import LagKlubbSide from './LagKlubbSide';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from "./components/Header";
+
 
 function App() {
   return (
       <Router>
         <div className="App">
+          <Header />
           <div className="innhold">
               <Switch>
                 <Route exact path="/lagKlubb">
---
+++ disk-applikasjon/frontend/src/components/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+    <header class="header"> 
+      <span class="header-title">DiscGolf</span>
+      <img 
+        class="logo"
+        src="https://cdn.discordapp.com/attachments/934547779773153340/1333095097125961840/G2KuNAhXAAAAABJRU5ErkJggg.png?ex=6797a50e&is=6796538e&hm=4268bfd85ba6c78c4b5c540071b84950c59720990a1e0653cabe71a1fc10bb29&" 
+        alt="logo"
+      />
+      <nav class="nav">
+        <ul>
+          <li><a href="#">Hjem</a></li>
+          <li><a href="#">Baner</a></li>
+          <li><a href="#">Regler/Tips</a></li>
+          <li><a href="#">Klubber</a></li>
+          <li><a href="#">Nyheter</a></li>
+          <li><a href="#">Logg inn</a></li>
+        </ul>
+      </nav>
+
+
+
+    </header>
+   
+    
+        <div class="bilde">
+            <img 
+                src="https://images.unsplash.com/photo-1616840388998-a514fe2175b9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+                alt="bilde"/>
+        </div>
+    </div>
+   
+  );
+};
+
+export default Header;
