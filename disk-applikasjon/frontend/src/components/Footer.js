ec29be857862fc0d0f7ecba5696e6eea67c05dd5
Abdinasir909 <abdinasirali24@gmail.com>
2025-01-28 15:21:30 +0100

    forbedret footer-stil og layout med grid-system, oppdatert CSS-klasser og struktur

--- disk-applikasjon/frontend/src/components/Footer.js
+++ disk-applikasjon/frontend/src/components/Footer.js
@@ -9,7 +9,25 @@ const Footer = () => {
                 <p>Registrer resultater og forbedre din spillopplevelse</p>
             </div>
 
-            <div className="footer2">
+           
+
+
+
+            <div className="footer">
+        
+                <ul>
+                    <li>
+                        
+            <h1 className="t2">DiscGolf</h1>
+                        <p>Disc golf er en spennende utendørs sport der målet er å kaste en
+                            frisbee i en kurv på færrest mulig kast. Med over 18 000 baner verden over,
+                            er sporten i vekst og passer for spillere i alle aldre og ferdighetsnivåer.</p>
+                    </li>
+
+                </ul>
+
+                <ul>
+                <div className="footer2">
                 <ul>
                     <li><a href="#">Baner</a></li>
                     <li><a href="#">Regler/Tips</a></li>
@@ -17,23 +35,11 @@ const Footer = () => {
                     <li><a href="#">Nyheter</a></li>
                 </ul>
             </div>
-
-            <h1 className="t2">DiscGolf</h1>
-
-
-            <div className="footer">
-                <ul>
-                    <li>
-                        <p>Disc golf er en spennende utendørs sport der målet er å kaste en
-                            frisbee i en kurv på færrest mulig kast. Med over 18 000 baner verden over,
-                            er sporten i vekst og passer for spillere i alle aldre og ferdighetsnivåer.</p>
-                    </li>
-                </ul>
-
-                <ul>
-                    <li>
+            <li class="copyr">
                         <p>© 2024 DiscGolf. All Rights Reserved.</p>
                     </li>
+
+                    
                 </ul>
 
                 <div className="info">
