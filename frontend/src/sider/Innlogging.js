5176d677f6767681ab1bc9d84c59589db666ed63
Abdinasir909 <abdinasirali24@gmail.com>
2025-01-29 23:33:24 +0100

    Forbedret stil på innloggingsskjemaet

--- frontend/src/sider/Innlogging.js
+++ frontend/src/sider/Innlogging.js
@@ -48,7 +48,7 @@ const Innlogging = () => {
                 value={bruker}
                 onChange={(e) => setBruker(e.target.value)}
                 required
-                className="px-5 py-3 m-2 border border-gray-300 rounded-full w-full focus:outline-none focus:ring-2 focus:ring-gray-400"
+                className="px-4 py-3 mb-3 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-400"
             />
             <input
                 type="password"
@@ -56,11 +56,11 @@ const Innlogging = () => {
                 value={passord}
                 onChange={(e) => setPassord(e.target.value)}
                 required
-                className="px-5 py-3 m-2 border border-gray-300 rounded-full w-full focus:outline-none focus:ring-2 focus:ring-gray-400"
+                className="px-5 py-3 m-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-gray-400"
             />
             <button
                 type="submit"
-                className="bg-gray-600 text-black border-radius-2 px-4 py-2 mt-4 rounded-full w-full border border-gray-500;"
+                className="bg-gray-600 text-black border-radius-2 px-4 py-2 mt-4 rounded-lg w-full border border-gray-500"    
             >
                 Logg inn
             </button>
