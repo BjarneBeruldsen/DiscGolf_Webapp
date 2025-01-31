9068873ad0b4091e00d7c348be07c1a8c7a06778
Bjarne Beruldsen <beruldsen.bjarne@gmail.com>
2025-01-31 09:43:54 +0100

    fikser refresh

--- backend/app.js
+++ backend/app.js
@@ -33,9 +33,6 @@ app.use(cors({
 //Deployment under
 
 //legger serving fra statiske filer fra REACT applikasjonen 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
@@ -309,5 +306,9 @@ app.delete('/tommeTestdata', (req, res) => {
         });
 });
 
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
 
 
