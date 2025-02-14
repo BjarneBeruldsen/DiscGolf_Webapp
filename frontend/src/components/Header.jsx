bdd4f962d3614fa552731dd3cc52cf37e0a80a32
Bjarne Beruldsen <beruldsen.bjarne@gmail.com>
2025-02-14 09:17:02 +0100

    tatt tilbake endringer

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -66,11 +66,9 @@ const Header = ({ loggetInnBruker, setLoggetInnBruker }) => {
           </ul>
         </nav>
 
-        <div className="block lg:hidden">
+        <div className="lg:hidden">
           <button onClick={toggleMeny} className="text-black font-bold hover:bg-gray-200 border rounded-lg shadow">
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-            </svg>
+            Meny
           </button>
         </div>
       </header>
