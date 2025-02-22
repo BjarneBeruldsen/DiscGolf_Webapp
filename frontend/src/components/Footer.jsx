b908e868d884f401a11d612e5283e3aa6afbc317
Abdinasir909 <abdinasirali24@gmail.com>
2025-02-22 01:27:39 +0100

    Fiks

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -28,7 +28,7 @@ const Footer = () => {
           </ul>
         </div>
 
-        <div className="text-right">
+        <div className="text-right justify-items-center">
           <p className="font-semibold">Kontakt oss</p>
           <p>Telefon: 1256789</p>
           <p>Email: DiscgolfBø@gmail.com</p>
