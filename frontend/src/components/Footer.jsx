148f66505a6db89196c909834c602378857a77dc
Abdinasir909 <abdinasirali24@gmail.com>
2025-02-22 17:39:40 +0100

    Fiks

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -10,32 +10,29 @@ const Footer = () => {
       </div>
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 px-8 py-6">
-        
-        <div className="text-left">
+        <div>
           <h1 className="text-xl font-bold">DiscGolf</h1>
-          <p className="mt-2 text-left">
-            Disc golf er en spennende utendørs sport der målet er å kaste en frisbee i en kurv 
-            på færrest mulig kast. Med over 18,000 baner verden over, er sporten i vekst og 
-            passer for spillere i alle aldre og ferdighetsnivåer.
+          <p className="mt-2">
+            Disc golf er en spennende utendørs sport der målet er å kaste en frisbee i en kurv på færrest mulig kast.
+            Med over 18,000 baner verden over, er sporten i vekst og passer for spillere i alle aldre og ferdighetsnivåer.
           </p>
         </div>
-        
+
+       
         <div className="text-center">
-          <ul className="inline-block space-x-6 text-center ">
-          <Link to="/Hjem" className="hover:text-gray-700 font-semibold">Hjem</Link>
-          <Link to="/OmOss" className="hover:text-gray-700 font-semibold">Om oss</Link>
-          <Link to="/Personvern" className="hover:text-gray-700 font-semibold">Personvern</Link>
-          <Link to="/Sikkerhet" className="hover:text-gray-700 font-semibold">Sikkerhet</Link>
+          <ul className="inline-block space-x-6 text-lg font-medium">
+            <li className="inline"><Link to="/Hjem" className="hover:text-gray-800">Hjem</Link></li>
+            <li className="inline"><Link to="#" className="hover:text-gray-800">Om oss</Link></li>
+            <li className="inline"><Link to="#" className="hover:text-gray-800">Personvern</Link></li>
+            <li className="inline"><Link to="#" className="hover:text-gray-800">Sikkerhet</Link></li>
           </ul>
         </div>
         
-        <div className="text-right">
-          <div className="justify-items-center">
+        <div className="text-right justify-items-center">
           <p className="font-semibold">Kontakt oss</p>
           <p>Telefon: 1256789</p>
           <p>Email: DiscgolfBø@gmail.com</p>
-          <p>Bø, Norway</p>
-          </div>
+          <p>BØ, Norway</p>
         </div>
       </div>
 
