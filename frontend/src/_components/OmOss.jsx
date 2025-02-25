0e60130989c7be571fbd03985fa03def964b202f
Abdinasir909 <abdinasirali24@gmail.com>
2025-02-25 17:26:10 +0100

    Liten endring

--- frontend/src/_components/OmOss.jsx
+++ frontend/src/_components/OmOss.jsx
@@ -4,7 +4,7 @@ const OmOss = () => {
   return (
     <div className="min-h-screen bg-white py-8">
       <div className="max-w-4xl mx-auto p-8">
-        <h1 className="text-3xl font-bold text-center mb-6">Om Oss</h1>
+        <h1 className="text-3xl font-bold mb-6">Om Oss</h1>
         <div className="space-y-6">
          
           <section>
