8651d8bf57d104bb8577ab2498a9457ce54bb494
Bjarne Beruldsen <beruldsen.bjarne@gmail.com>
2025-03-03 08:50:21 +0100

    flyttet "tøm test data til midten"

--- frontend/src/_components/Hjem.jsx
+++ frontend/src/_components/Hjem.jsx
@@ -111,7 +111,7 @@ const Hjem = () => {
 </div>
 
   
-     <div className="mt-8 grid">
+     <div className="m-8 flex justify-center">
             <button onClick={handleTommeTestdata} className="py-4 px-8 bg-red-500 rounded-lg text-sm text-white justify-self-end">
                 Tøm testdata
             </button>
