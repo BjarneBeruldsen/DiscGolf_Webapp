44898ed924744ec2f5b67e8aea2fe37f63dc7d2a
Bjarne Beruldsen <beruldsen.bjarne@gmail.com>
2025-03-31 16:46:36 +0200

    lagt til statistikk bokser

--- frontend/src/KlubbHandtering/Poengtavler.jsx
+++ frontend/src/KlubbHandtering/Poengtavler.jsx
@@ -5,44 +5,69 @@ const PoengTavler = () => {
 
     const { bruker, venter } = HentBruker();
     const [poengTavler, setPoengTavler] = useState([]);
+    const [visStatistikk, setVisStatistikk] = useState(true);
+    const [visPoengtavler, setVisPoengtavler] = useState(false);
+    const [valgtSeksjon, setValgtSeksjon] = useState('poengtavler');
+    const [brukernavn, setBrukernavn] = useState('');   
 
     useEffect(() => {
         if (bruker && bruker.poengkort) {
             setPoengTavler(bruker.poengkort);
+            setBrukernavn(bruker.brukernavn);
             console.log(bruker); 
         }
     }, [bruker]);
+
+    const handleVis = (seksjon) => {
+        setValgtSeksjon(seksjon);
+        setVisPoengtavler(seksjon === 'poengtavler');
+        setVisStatistikk(seksjon === 'statistikk');
+    };
    
     return (
-        <div className="poengtavler flex justify-center bg-gray-200">
-            <div className="grid grid-cols-1 lg-grid-cols-2 gap-6">
+        <div className="statistikk bg-gray-200">
+            <div className='topp-panel bg-white p-4 shadow w-full'>
+                <div className='overskrift border-b'>
+                    <h2 className="text-3xl font-bold">{ brukernavn }sin statistikk</h2>
+                </div>
+                <div className='navbar p-2'>
+                    <button onClick={() => handleVis('statistikk')} className={`justify-center py-2 px-2 m-2 text-sm ${valgtSeksjon === 'statistikk' ? 'border-b-2 border-black-500' : 'rounded-lg bg-white text-gray hover:bg-gray-200'}`}>Statistikk</button>
+                    <button onClick={() => handleVis('poengtavler')} className={`justify-center py-2 px-2 m-2 text-sm ${valgtSeksjon === 'poengtavler' ? 'border-b-2 border-black-500' : 'rounded-lg bg-white text-gray hover:bg-gray-200'}`}>Poengtavler</button>
+                </div>
+            </div>
             {venter && <div>Laster...</div>}
+            {visPoengtavler ? ( 
+            <div className="grid grid-cols-1 lg-grid-cols-2 gap-6"><div>
             {poengTavler && poengTavler.length > 0  ? (
                 poengTavler.map((poengTavle, index) => (
                     <div className="oppsummering bg-white shadow rounded-lg m-8 border p-5" key={index}>
                         <h2 className="text-center font-bold text-xl mb-4">Poengkort fra: {poengTavle.nyPoengkort.baneNavn}</h2>
-                        <table className="border-separate border rounded-lg border-gray-400">
-                            <thead>
-                                <tr>
-                                    <th className="py-2">Spiller</th>
-                                    <th className="py-2">Total Poengsum</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {poengTavle.nyPoengkort && poengTavle.nyPoengkort.spillere ? (
-                                    poengTavle.nyPoengkort.spillere.map(spiller => (
-                                        <tr key={spiller.id}>
-                                            <td className="border px-4 py-2">{spiller.navn}</td>
-                                            <td className="border px-4 py-2">{spiller.total}</td>
-                                        </tr>
-                                    ))
-                                ) : (
+                        <div className="relative flex flex-col w-full bg-white shadow-md rounded-xl">
+                            <table className="w-full text-left table-auto min-w-max">
+                                <thead>
                                     <tr>
-                                        <td colSpan="2" className="border px-4 py-2">Ingen spillere tilgjengelig</td>
+                                        <th className="p-4 border-b bg-gray-200 rounded-tl-xl">Spiller</th>
+                                        <th className="p-4 border-b bg-gray-200">Antall kast</th>
+                                        <th className="p-4 border-b bg-gray-200 rounded-tr-xl">Total Poengsum</th>
                                     </tr>
-                                )}
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                    {poengTavle.nyPoengkort && poengTavle.nyPoengkort.spillere ? (
+                                        poengTavle.nyPoengkort.spillere.map(spiller => (
+                                            <tr className="even:bg-gray-100" key={spiller.id}>
+                                                <td className="p-4">{spiller.navn}</td>
+                                                <td className="p-4">{spiller.total}</td>
+                                                <td className="p-4">{spiller.total}</td>
+                                            </tr>
+                                        ))
+                                    ) : (
+                                        <tr>
+                                            <td colSpan="2" className="border px-4 py-2">Ingen spillere tilgjengelig</td>
+                                        </tr>
+                                    )}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 ))
             ) : (
@@ -50,7 +75,41 @@ const PoengTavler = () => {
                     <h1>Ingen poengtavler tilgjengelig...</h1>
                 </div>
             )}
+            </div> 
             </div>
+            ) : (
+                <div className="flex justify-center">
+                    <div className="statistikk bg-white shadow rounded-xl m-8 p-5 w-full">
+                        <p>kommer senere..</p>
+                    <div className="grid md:grid-cols-3 gap-6">
+                            <div className="bg-gray-100 shadow-md rounded-xl p-4">
+                                <p className="text-sm">Antall runder</p>
+                                <p className="text-xl font-bold">100</p>
+                            </div>
+                            <div className="bg-gray-100 shadow-md rounded-xl p-4">
+                                <p className="text-sm">Gjennomsnitlig score</p>
+                                <p className="text-xl font-bold">100</p>
+                            </div>
+                            <div className="bg-gray-100 shadow-md rounded-xl p-4">
+                                <p className="text-sm">Antall kast totalt</p>
+                                <p className="text-xl font-bold">100</p>
+                            </div>
+                            <div className="bg-gray-100 shadow-md rounded-xl p-4">
+                                <p className="text-sm">Gjennomsnitlig kast pr.runde</p>
+                                <p className="text-xl font-bold">100</p>
+                            </div>
+                            <div className="bg-gray-100 shadow-md rounded-xl p-4">
+                            <p className="text-sm">Beste score</p>
+                            <p className="text-xl font-bold">100</p>
+                            </div>
+                            <div className="bg-gray-100 shadow-md rounded-xl p-4">
+                            <p className="text-sm">Antall hole in one</p>
+                            <p className="text-xl font-bold">100</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
