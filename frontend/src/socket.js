c7787395f85ce4b4a759ae48fe50f79375c61599
Bjarne Beruldsen <beruldsen.bjarne@gmail.com>
2025-04-10 13:52:52 +0200

    endret url

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // Opprett socket-tilkobling
-const socket = io('http://localhost:8000', {
+const socket = io(process.env.REACT_APP_API_BASE_URL, {
     withCredentials: true,
 });
 
